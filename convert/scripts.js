7cc9e1398ebd5af90f63ecb9124c6574a90d516c
douglasdl <douglas_san@hotmail.com>
2024-05-08 19:16:33 -0300

    feat: validate the input value

--- convert/scripts.js
+++ convert/scripts.js
@@ -1,5 +1,7 @@
 const amount = document.getElementById("amount");
 
 amount.addEventListener("input", () => {
+  const hasRegexCharacteres = /\D+/g
+  amount.value = amount.value.replace(hasRegexCharacteres, "");
   console.log(amount.value);
 })
