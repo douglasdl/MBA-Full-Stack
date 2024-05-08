3d177cb3be9f4a2e598d50fe9c26fde2f15c4a9a
douglasdl <douglas_san@hotmail.com>
2024-05-08 19:29:50 -0300

    feat: get the selected currency

--- convert/scripts.js
+++ convert/scripts.js
@@ -1,7 +1,15 @@
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
 
 amount.addEventListener("input", () => {
   const hasRegexCharacteres = /\D+/g
   amount.value = amount.value.replace(hasRegexCharacteres, "");
   console.log(amount.value);
 })
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  console.log(currency.value);
+}
