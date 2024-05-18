4b5945e3abdf2b8fd34af3e3e69fe4380207a9e4
douglasdl <douglas_san@hotmail.com>
2024-05-17 22:11:30 -0300

    feat: identify the currency and show the footer

--- convert/scripts.js
+++ convert/scripts.js
@@ -1,6 +1,14 @@
+
+// const USD = await fetch("https://api.aoikujira.com/kawase/json/usd");
+const USD = 5.17;
+const EUR = 5.35;
+const GBP = 5.78;
+
+
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
 
 amount.addEventListener("input", () => {
   const hasRegexCharacteres = /\D+/g
@@ -11,5 +19,27 @@ amount.addEventListener("input", () => {
 form.onsubmit = (event) => {
   event.preventDefault();
 
-  console.log(currency.value);
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break;
+    default:
+      break;
+  }
+}
+
+function convertCurrency(amount, price, symbol) {
+  try {
+    footer.classList.add("show-result");
+  } catch (error) {
+    footer.classList.remove("show-result");
+    console.log(error);
+    alert("ERRO", "Não foi possível converter. Tente novamente mais tarde.");
+  }
 }
