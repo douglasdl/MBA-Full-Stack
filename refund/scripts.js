4707766af41fc033aeb961134cee2fc21581fb69
douglasdl <douglas_san@hotmail.com>
2024-06-12 11:38:37 -0300

    feat: add the expanse value

--- refund/scripts.js
+++ refund/scripts.js
@@ -1,9 +1,14 @@
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+
+const expenseList = document.querySelector("ul");
+
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
   value = Number(value) / 100;
   amount.value = formatCurrencyBRL(value);
-  console.log(value);
 };
 
 function formatCurrencyBRL(value) {
@@ -12,4 +17,55 @@ function formatCurrencyBRL(value) {
     currency: "BRL"
   });
   return value;
+}
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    create_at: new Date() 
+  }
+
+  addExpense(newExpense);
+}
+
+function addExpense(newExpense) {
+  try {
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+
+    expenseItem.append(expenseIcon);
+    expenseList.append(expenseItem);
+
+    const expenseInfo = document.createElement("div");
+    expenseInfo.classList.add("expense-info");
+
+    const expenseName = document.createElement("strong");
+    expenseName.textContent = newExpense.expense;
+
+    const expenseCategory = document.createElement("span");
+    expenseCategory.textContent = newExpense.category_name;
+
+    expenseInfo.append(expenseName, expenseCategory);
+
+    const expenseAmount = document.createElement("span");
+    expenseAmount.classList.add("expense-amount");
+    expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`;
+
+
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount);
+    expenseList.append(expenseItem);
+  } catch (error) {
+    alert("Não foi possível atualizar a lista de despesas.");
+    console.log(error);
+  }
 }
