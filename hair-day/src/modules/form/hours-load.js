48c0a1b3193fec75f2813ca54e809395aa9977cd
douglasdl <douglas_san@hotmail.com>
2024-07-21 18:56:02 -0300

    feat: render the hours

--- hair-day/src/modules/form/hours-load.js
+++ hair-day/src/modules/form/hours-load.js
@@ -1,6 +1,8 @@
 import dayjs from 'dayjs'
 import { openingHours } from '../../utils/opening-hours.js'
 
+const hours = document.getElementById('hours')
+
 export function hoursLoad({ date }) {
   const opening = openingHours.map(hour => {
     const [scheduleHour] = hour.split(':')
@@ -11,5 +13,12 @@ export function hoursLoad({ date }) {
       available: isFutureHour
     }
   })
-  // console.log(opening)
+
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement('li')
+    li.classList.add('hour')
+    li.classList.add(available ? 'hour-available' : 'hour-unavailable')
+    li.textContent = hour
+    hours.appendChild(li)
+  })
 }
