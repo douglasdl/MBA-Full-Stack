acc473345acc5a5a85daf488c57dbe0e82a10a61
douglasdl <douglas_san@hotmail.com>
2024-07-22 19:03:28 -0300

    feat: send the id, name, date and time to the api

--- hair-day/src/modules/form/submit.js
+++ hair-day/src/modules/form/submit.js
@@ -2,6 +2,7 @@ import dayjs from 'dayjs'
 
 const form = document.querySelector('form')
 const selectedDate = document.getElementById('date')
+const clientName = document.getElementById('client')
 
 const today = dayjs(new Date()).format('YYYY-MM-DD')
 selectedDate.value = today
@@ -10,5 +11,29 @@ selectedDate.min = today
 form.onsubmit = event => {
   event.preventDefault()
 
-  console.log('enviado')
+  try {
+    const name = clientName.value.replace(/\s+/g, ' ')
+    if (!name) {
+      return alert('Informe o nome do client!')
+    }
+
+    const selectedHour = document.querySelector('.hour-selected')
+
+    if (!selectedHour) {
+      return alert('Selecione a hora!')
+    }
+
+    const [hour] = selectedHour.innerHTML.split(':')
+
+    const when = dayjs(selectedDate.value).add(hour, 'hour')
+    const id = new Date().getTime()
+    console.log({
+      id,
+      name,
+      when
+    })
+  } catch (error) {
+    alert('Não foi possível realizar o agendamento!')
+    console.log(error)
+  }
 }
