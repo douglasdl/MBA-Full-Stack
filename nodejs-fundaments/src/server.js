94363978c9625a3911e36d09d8e5dec4ae62cdc5
douglasdl <douglas_san@hotmail.com>
2024-08-28 12:21:47 -0300

    feat: save users in memory

--- nodejs-fundaments/src/server.js
+++ nodejs-fundaments/src/server.js
@@ -1,13 +1,23 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((request, response) => {
   const { method, url } = request
 
   if(method === 'GET' && url === '/users') {
-    return response.end('Listing users')
+    return response
+      .setHeader('Content-type', 'application/json')  
+      .end(JSON.stringify(users))
   }
   
   if(method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'Douglas Dias Leal',
+      email: 'douglas_san@hotmail.com'
+    })
+
     return response.end('Creating users')
   }
 
