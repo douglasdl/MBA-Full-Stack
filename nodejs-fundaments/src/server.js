d3da6b1d10a97e3aad290d471397b4d40d3404f9
douglasdl <douglas_san@hotmail.com>
2024-08-28 12:08:49 -0300

    feat: add routes wirh method and url

--- nodejs-fundaments/src/server.js
+++ nodejs-fundaments/src/server.js
@@ -1,10 +1,16 @@
-// CommonJS
-// const http = require('http')
-
-// ESModules
 import http from 'node:http'
 
 const server = http.createServer((request, response) => {
+  const { method, url } = request
+
+  if(method === 'GET' && url === '/users') {
+    return response.end('Listing users')
+  }
+  
+  if(method === 'POST' && url === '/users') {
+    return response.end('Creating users')
+  }
+
   return response.end('Hello World')
 })
 
