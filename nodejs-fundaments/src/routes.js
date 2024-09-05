6c89079551b3c2017249fdb182bfeda45d391129
douglasdl <douglas_san@hotmail.com>
2024-09-05 06:45:23 -0300

    feat: route, query and params

--- nodejs-fundaments/src/routes.js
+++ nodejs-fundaments/src/routes.js
@@ -27,5 +27,12 @@ export const routes = [
 
       return response.writeHead(201).end()
     }
-  }
+  },
+  {
+    method: 'DELETE',
+    path: '/users',
+    handler: (request, response) => {
+      return response.end()
+    }
+  },
 ]
--- nodejs-fundaments/src/server.js
+++ nodejs-fundaments/src/server.js
@@ -2,7 +2,15 @@ import http from 'node:http'
 import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
 
+// Query Parameters: URL stateful => filter, pagination, optional info
+//   http://localhost:3333/users?userId=1&name=Douglas
 
+// Route Parameters: resource identification
+//   GET    http://localhost:3333/users/1
+
+// DELETE http://localhost:3333/users/1
+// Request Body: send form info (HTTPS)
+// POST   http://localhost:3333/users
 
 const server = http.createServer(async (request, response) => {
   const { method, url } = request
