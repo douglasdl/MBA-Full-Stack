4a47c2a1f85409181566308b41c776d6cd6c7309
douglasdl <douglas_san@hotmail.com>
2024-09-06 03:18:55 -0300

    feat: update the Post component structure

--- ignite-feed/src/components/Post.jsx
+++ ignite-feed/src/components/Post.jsx
@@ -1,8 +1,23 @@
+import styles from './Post.module.css'
+
 export function Post({ author, content}) {
   return (
-    <div>
-      <strong>{author}</strong>
-      <p>{content}</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}></div>
+        <img className={styles.avatar} src="https://github.com/douglasdl.png" alt="Avatar" />
+        <div className={styles.authorInfo}>
+          <strong>{author}</strong>
+          <span>Web Developer</span>
+        </div>
+        <time title="06 de Setembro às 15:10:00" dateTime="2024-09-06 15:10:00">Publicado há 1h</time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifolio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉 <a href="#">douglasdl/doctorcare</a></p>
+        <p><a href="#">#novoprojeto</a> <a href="#">#nlw</a> <a href="#">#rocketseat</a></p>
+      </div>
+    </article>
   )  
 }
