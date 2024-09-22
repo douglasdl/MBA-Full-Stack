2f058595820e08badb954b00a003782743f2ef6f
douglasdl <douglas_san@hotmail.com>
2024-09-22 19:54:34 -0300

    feat: add the like function

--- ignite-feed/src/components/Comment.jsx
+++ ignite-feed/src/components/Comment.jsx
@@ -1,10 +1,17 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
+import { useState } from 'react';
+
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
 
   function handleDeleteComment() {
     onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -26,10 +33,10 @@ export function Comment({ content, onDeleteComment }) {
           </p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
           </button>
-          Aplaudir ・<span>20</span>
+          Aplaudir ・<span>{likeCount}</span>
         </footer>
       </div>
     </div>
