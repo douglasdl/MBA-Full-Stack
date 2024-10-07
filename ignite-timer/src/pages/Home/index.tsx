5c4d0dfceea0a9a46f513d8252ca7e1c9edfa97d
douglasdl <douglas_san@hotmail.com>
2024-10-07 11:35:39 -0300

    refactor: fix form reset

--- ignite-timer/src/pages/Home/index.tsx
+++ ignite-timer/src/pages/Home/index.tsx
@@ -28,7 +28,12 @@ export function Home() {
     }
   })
 
-  const { handleSubmit, watch, /*reset*/ } = newCycleForm;
+  const { handleSubmit, watch, reset } = newCycleForm;
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data);
+    reset();
+  }
   
 
   const task = watch('task');
@@ -38,7 +43,7 @@ export function Home() {
 
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(createNewCycle)} action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormProvider {...newCycleForm}>
           <NewCycleForm />
         </FormProvider>
