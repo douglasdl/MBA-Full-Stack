5498ec8aeaadb26a1cbae15067385b2d882bb88c
douglasdl <douglas_san@hotmail.com>
2024-10-19 19:16:30 -0300

    feat: add not found page

--- pizzashop-web/src/routes.tsx
+++ pizzashop-web/src/routes.tsx
@@ -5,11 +5,13 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { SignUp } from './pages/auth/sign-up'
 import { Orders } from './pages/app/orders/orders'
+import { NotFountd } from './pages/404'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFountd />,
     children: [
       { path: '/', element: <Dashboard />},
       { path: '/orders', element: <Orders />},
