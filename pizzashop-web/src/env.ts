2c26e3b56f7de83de8f68c22f5cacd01b4cdca3b
douglasdl <douglas_san@hotmail.com>
2024-11-02 19:58:16 -0300

    remove supabase

--- pizzashop-web/src/env.ts
+++ pizzashop-web/src/env.ts
@@ -1,15 +1,8 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-  USE_API: z.string(),
-  DATABASE_URL: z.string().url(),
-  DIRECT_URL: z.string().url(),     
-  GOOGLE_CLIENT_ID: z.string(),
-  GOOGLE_CLIENT_SECRET: z.string(),
-  NEXT_AUTH_SECRET: z.string(),
-  VITE_SUPABASE_URL: z.string().url(),
-  VITE_SUPABASE_ANON_KEY: z.string(),
-  SUPABASE_KEY: z.string()
+  VITE_API_URL: z.string(),
+  VITE_ENABLE_API_DELAY: z.string().transform((value) => value === 'true')
 })
 
 export const env = envSchema.parse(import.meta.env)
--- pizzashop-web/src/lib/axios.ts
+++ pizzashop-web/src/lib/axios.ts
@@ -2,6 +2,6 @@ import { env } from "@/env"
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: env.VITE_SUPABASE_URL,
+  baseURL: env.VITE_API_URL,
   withCredentials: true
 })
