329bc8051f2fa547b2c18dddcdb8b192b31e16f7
douglasdl <douglas_san@hotmail.com>
2024-11-03 19:21:04 -0300

    feat: add knex select all query

--- api-rest-nodejs/src/server.ts
+++ api-rest-nodejs/src/server.ts
@@ -4,15 +4,9 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
+  const transactions = await knex('transactions').select('*')
 
-  return transaction
+  return transactions
 })
 
 app
