4a8838dc608b5798c15a0646ac38ba1afbf035d9
douglasdl <douglas_san@hotmail.com>
2024-11-21 19:17:16 -0300

    feat: add tests database settings

--- api-rest-nodejs/src/env/index.ts
+++ api-rest-nodejs/src/env/index.ts
@@ -1,17 +1,23 @@
-import 'dotenv/config'
-import { z } from 'zod'
+import { config } from "dotenv";
+import { z } from "zod";
 
-const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
-  DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
-})
-
-export const _env = envSchema.safeParse(process.env)
-
-if (_env.success === false) {
-  console.error('⚠️ Invalid environment variables!', _env.error.format())
-  throw new Error('Invalid environment variables!')
+if (process.env.NODE_ENV === "test") {
+  config({ path: ".env.test" });
+} else {
+  config();
 }
 
-export const env = _env.data
+const envSchema = z.object({
+  NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
+  DATABASE_URL: z.string(),
+  PORT: z.number().default(3333),
+});
+
+export const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error("⚠️ Invalid environment variables!", _env.error.format());
+  throw new Error("Invalid environment variables!");
+}
+
+export const env = _env.data;
