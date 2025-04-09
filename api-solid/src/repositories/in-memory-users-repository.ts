8493d0d77caa050994ddf417fbad5a4155c34f3a
Douglas Dias Leal <douglas_san@hotmail.com>
2025-04-09 23:56:06 +0900

    feat: add repository interface

--- api-solid/src/repositories/in-memory-users-repository.ts
+++ api-solid/src/repositories/in-memory-users-repository.ts
@@ -1,9 +1,18 @@
-import { prisma } from '@/lib/prisma'
-import type { Prisma } from '@prisma/client'
+import type { Prisma, User } from '@prisma/client'
+import type { UsersRepository } from './users-repository'
 
-export class InMemoryUsersRepository {
-  public users: any[] = []
-  async create(data: Prisma.UserCreateInput) {
-    this.users.push(data)
+export class InMemoryUsersRepository implements UsersRepository {
+  public users: User[] = []
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    const user: User = {
+      id: (Math.random() * 1000).toString(), // Generate a mock ID
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    }
+    this.users.push(user)
+    return user
   }
 }
