878b2258105fad92fec39117b176dfba3e9761fc
Douglas Dias Leal <douglas_san@hotmail.com>
2025-04-11 05:31:03 +0900

    feat: add register unit test

--- api-solid/src/http/use-cases/register.spec.ts
+++ api-solid/src/http/use-cases/register.spec.ts
@@ -1,5 +1,35 @@
+import { describe } from 'node:test'
+import { compare } from 'bcryptjs'
 import { expect, test } from 'vitest'
+import { RegisterUseCase } from './register'
 
-test('2 + 2 = 4', () => {
-  expect(2 + 2).toBe(4)
+describe('Register use case', () => {
+  test('should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return null
+      },
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'Douglas Dias Leal',
+      email: 'douglas_san@hotmail.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash
+    )
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
 })
